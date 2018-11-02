6a9761c35e4b200f86f9872aa0bbf4522b4f1433
Jack <jack.woodhouse.97@gmail.com>
2018-11-02 13:00:54 +0000

    basic
    basic

--- Main.py
+++ Main.py
@@ -1,2 +1,15 @@
-from nltk.corpus import stopwords
-print(stopwords.words('english'))
+# from nltk.corpus import stopwords
+# print(stopwords.words('english'))
+
+
+message = ("hello world")
+print(message)
+
+
+string1 = 'hello'
+
+string2 = "hello"
+
+final = string1 + " " + string2
+
+print("this is a concatenation using to upper " + final.upper() + "!")
