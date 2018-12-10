98ecf3142c5f25913e83cff16c81b7d344a3cc4f
Jack <jack.woodhouse.97@gmail.com>
2018-12-10 14:28:21 +0000

    j

--- Main.py
+++ Main.py
@@ -12,9 +12,26 @@ string2 = "hello"
 
 final = string1 + " " + string2
 
-print("this is a concatenation using to upper " + string1.rstrip() + string2.rstrip() + "!")
+print("this is using to upper and strips white space" + string1.rstrip() + string2.rstrip() + "!")
 
 
 age = 23
 
 print("happy " + str(age) + "rd birthday")
+
+bikes = ['trek', 'redline','mongo', ]
+
+print(bikes)
+
+
+print(bikes[2].title())
+
+bikes.append('new bike name')
+
+
+print(bikes)
+
+bikes.insert(3,' new second bike')
+
+
+print(bikes)
