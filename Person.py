3d4421474fed2cdd8a2a956f6948da6e43de8150
jackwoodhouse <jack.woodhouse.97@gmail.com>
2019-01-07 17:29:36 +0000

    added basic user class
    NEED TO ADD INPUT VERIFICATION TOWARDS THE END

--- Person.py
+++ Person.py
@@ -5,13 +5,15 @@ class User:
 
     def __init__(self, firstName, lastName, age, location, gender):
 
-        self.uniqueId = User.class_counter
-        User.class_counter += 1
+
         self.firstName = firstName
         self.lastName = lastName
         self.age = age
         self.location = location
         self.gender = gender
+
+        self.uniqueId = User.class_counter
+        User.class_counter += 1
 
     def displayUser(self):
         print("ID : ", self.uniqueId, "First Name : ", self.firstName, "Last Name : ", self.lastName, "Age : ", self.age, "location : ", self.location, " Gender : ", self.gender)
