8a46a82610d33dcbfa13b4a23334b2ad83e59bba
Jack <jack.woodhouse.97@gmail.com>
2019-01-10 14:33:05 +0000

    tokenize working

--- Person.py
+++ Person.py
@@ -8,13 +8,11 @@ class User:
 
         self.firstName = firstName
         self.lastName = lastName
-        self.age = age
-        self.location = location
-        self.gender = gender
+
 
         self.uniqueId = User.class_counter
         User.class_counter += 1
 
     def displayUser(self):
-        print("ID : ", self.uniqueId, "First Name : ", self.firstName, "Last Name : ", self.lastName,
-              "Age : ", self.age, "location : ", self.location, " Gender : ", self.gender)
+        print("ID : ", self.uniqueId, "First Name : ", self.firstName, "Last Name : ", self.lastName)
+
