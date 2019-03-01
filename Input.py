f2b1778d8ce694337e433e40968d3776bb5a03b1
jackwoodhouse <jack.woodhouse.97@gmail.com>
2019-03-01 13:37:45 +0000

    added some basic word removal

--- Input.py
+++ Input.py
@@ -1,5 +1,6 @@
 import nltk
-from nltk import word_tokenize
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
 
 
 class Language:
@@ -12,5 +13,13 @@ class Language:
 
     def f1(self):
 
-        words = nltk.word_tokenize(self.Text)
-        return words
+        stop_words = set(stopwords.words("english"))
+        words = word_tokenize(self.Text)
+
+        filtered_sentence = []
+
+        for w in words:
+            if w not in stop_words:
+                filtered_sentence.append(w)
+
+        return filtered_sentence
